1247fb73c2b8cb99000376a584884812ba839d39
MoonHZ <58355523+tieusau12@users.noreply.github.com>
2021-07-19 15:18:20 +0700

    haund up student.module.ts

--- C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/student/student.module.ts
+++ C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/student/student.module.ts
@@ -6,10 +6,11 @@ import { ListStudentComponent } from './list-student/list-student.component';
 import { CreateStudentComponent } from './create-student/create-student.component';
 import { EditStudentComponent } from './edit-student/edit-student.component';
 import { DeleteStudentComponent } from './delete-student/delete-student.component';
+import { CreateStudentExcelComponent } from './create-student-excel/create-student-excel.component';
 
 
 @NgModule({
-  declarations: [ListStudentComponent, CreateStudentComponent, EditStudentComponent, DeleteStudentComponent],
+  declarations: [ListStudentComponent, CreateStudentComponent, EditStudentComponent, DeleteStudentComponent, CreateStudentExcelComponent],
   imports: [
     CommonModule,
     StudentRoutingModule
