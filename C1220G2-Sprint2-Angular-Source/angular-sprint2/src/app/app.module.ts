9db4966f3a37a226a979179534ae370565a681e6
Đức Hậu ( MoonHz) <58355523+tieusau12@users.noreply.github.com>
2021-07-19 15:51:14 +0700

    Revert "Create teacher excel"

--- C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/app.module.ts
+++ C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/app.module.ts
@@ -4,18 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import {TeacherModule} from './teacher/teacher.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        SharedModule,
-        TeacherModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/teacher/teacher.module.ts
+++ C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/teacher/teacher.module.ts
@@ -1,19 +1,18 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 import { TeacherRoutingModule } from './teacher-routing.module';
 import { CreateTeacherComponent } from './create-teacher/create-teacher.component';
 import { ListTeacherComponent } from './list-teacher/list-teacher.component';
 import { EditTeacherComponent } from './edit-teacher/edit-teacher.component';
 import { DeleteTeacherComponent } from './delete-teacher/delete-teacher.component';
-import { CreateTeacherExcelComponent } from './create-teacher-excel/create-teacher-excel.component';
 
 
 @NgModule({
-  declarations: [CreateTeacherComponent, ListTeacherComponent, EditTeacherComponent, DeleteTeacherComponent, CreateTeacherExcelComponent],
+  declarations: [CreateTeacherComponent, ListTeacherComponent, EditTeacherComponent, DeleteTeacherComponent],
   imports: [
     CommonModule,
     TeacherRoutingModule
   ]
 })
-export class TeacherModule {
-}
+export class TeacherModule { }
