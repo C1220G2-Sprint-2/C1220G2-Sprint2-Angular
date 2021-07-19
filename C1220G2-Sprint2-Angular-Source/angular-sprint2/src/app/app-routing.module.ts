5e1b205f1876e32117581c047076b01a41b37179
huyvmdn <huyvm.dn@gmail.com>
2021-07-19 16:48:47 +0700

    huyn

--- C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/app-routing.module.ts
+++ C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/app-routing.module.ts
@@ -7,20 +7,13 @@ const routes: Routes = [
 
     path: 'de-tai',
     loadChildren: () => import('./project/project.module').then(module => module.ProjectModule)
-  }
+  },
 
+  {
     path: 'nhom',
     loadChildren: () => import('./student-group/student-group.module').then(module => module.StudentGroupModule)
   },
-<<<<<<< HEAD
-  {
-    path: 'de-tai',
-    loadChildren: () => import('./project/project.module').then(module => module.ProjectModule)
-  },
-=======
 
-
->>>>>>> 3c078feac9ebfb8ffb3d38114fdfdcd61c6b9103
 ];
 
 @NgModule({
