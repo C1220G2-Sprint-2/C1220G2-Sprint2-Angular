7190a55fdc2eaab0a01628f56ad83249575d6917
MoonHZ <58355523+tieusau12@users.noreply.github.com>
2021-07-19 17:02:51 +0700

    haund update code version 1

--- C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/app-routing.module.ts
+++ C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/app-routing.module.ts
@@ -7,8 +7,14 @@ const routes: Routes = [
   {
     path: 'quan-ly-tien-do',
     loadChildren: () => import('./progress-management/progress-management.module').then(module => module.ProgressManagementModule)
-  }
+  },
   // end code by sang
+  // code by hau
+  {
+    path: 'bao-cao-tien-do',
+    loadChildren: () => import('./report-progress/report-progress.module').then(module => module.ReportProgressModule)
+  }
+  // end code by hau
 ];
 
 @NgModule({
