874e125ef9ed25ae37ba7f71828026a6c34bffd3
sirrin1991 <sirrin1991@gmail.com>
2021-07-19 14:27:20 +0700

    update_code_FE

--- C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/security/security-routing.module.ts
+++ C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/security/security-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {UserInformationComponent} from './user-information/user-information.component';
+import {ChangePasswordComponent} from './change-password/change-password.component';
+import {ForgotPasswordComponent} from './forgot-password/forgot-password.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'user/information', component: UserInformationComponent },
+  { path: 'user/changepassword', component: ChangePasswordComponent },
+  { path: 'user/forgotpassword', component: ForgotPasswordComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/security/security.module.ts
+++ C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/security/security.module.ts
@@ -7,10 +7,11 @@ import { LogoutComponent } from './logout/logout.component';
 import { ChangePasswordComponent } from './change-password/change-password.component';
 import { UserInformationComponent } from './user-information/user-information.component';
 import { ChangeAdminInformationComponent } from './change-admin-information/change-admin-information.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 
 @NgModule({
-  declarations: [LoginComponent, LogoutComponent, ChangePasswordComponent, UserInformationComponent, ChangeAdminInformationComponent],
+  declarations: [LoginComponent, LogoutComponent, ChangePasswordComponent, UserInformationComponent, ChangeAdminInformationComponent, ForgotPasswordComponent],
   imports: [
     CommonModule,
     SecurityRoutingModule
