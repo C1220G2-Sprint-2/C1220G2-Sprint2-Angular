41f258bda6a0d614ae6275ab0cb55f70b3c0069b
sangle173 <76678575+sangle173@users.noreply.github.com>
2021-07-24 21:42:19 +0700

    sangld update

--- C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/app.module.ts
+++ C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/app.module.ts
@@ -1,9 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {SharedModule} from './shared/shared.module';
+import {HttpClientModule} from '@angular/common/http';
+import {NgxPaginationModule} from 'ngx-pagination';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,9 +15,13 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule, // code by sang
+    NgxPaginationModule, // code by sang
+    NgbModule // code by sang
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/progress-management/progress-management.module.ts
+++ C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/progress-management/progress-management.module.ts
@@ -5,13 +5,15 @@ import { ProgressManagementRoutingModule } from './progress-management-routing.m
 import { ProgressTeamComponent } from './progress-team/progress-team.component';
 import { ProgressDetailComponent } from './progress-detail/progress-detail.component';
 import { ReviewComponent } from './review/review.component';
+import {NgbPaginationModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
   declarations: [ProgressTeamComponent, ProgressDetailComponent, ReviewComponent],
   imports: [
     CommonModule,
-    ProgressManagementRoutingModule
+    ProgressManagementRoutingModule,
+    NgbPaginationModule
   ]
 })
 export class ProgressManagementModule { }
