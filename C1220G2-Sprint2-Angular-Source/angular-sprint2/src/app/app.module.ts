8df11dece999b741dbf889c7e0a42e57a1061066
sangle173 <76678575+sangle173@users.noreply.github.com>
2021-07-24 22:15:16 +0700

    sangld update

--- C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/app.module.ts
+++ C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/app.module.ts
@@ -4,12 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import {NgxPaginationModule} from "ngx-pagination";
-import {HttpClientModule} from "@angular/common/http";
-import {StudentGroupModule} from "./student-group/student-group.module";
-import {ProjectModule} from "./project/project.module";
-import {NgxPaginationModule} from 'ngx-pagination';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgxPaginationModule} from 'ngx-pagination';
+import {HttpClientModule} from '@angular/common/http';
+import {StudentGroupModule} from './student-group/student-group.module';
+import {ProjectModule} from './project/project.module';
 
 @NgModule({
   declarations: [
@@ -19,7 +18,7 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    NgbModule // code by sang
+    NgbModule, // code by sang
     NgxPaginationModule,
     HttpClientModule,
     StudentGroupModule,
--- C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/student-group/student-group-routing.module.ts
+++ C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/student-group/student-group-routing.module.ts
@@ -1,17 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {ListStudentGroupComponent} from "./list-student-group/list-student-group.component";
-import {ProjectRegistrationComponent} from "../project/project-registration/project-registration.component";
-import {StudentGroupRegistrationComponent} from "./student-group-registration/student-group-registration.component";
-
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {ListStudentGroupComponent} from './list-student-group/list-student-group.component';
+import {StudentGroupRegistrationComponent} from './student-group-registration/student-group-registration.component';
 
 
 const routes: Routes = [
   {
-    path: "danh-sach",
+    path: 'danh-sach',
     component: ListStudentGroupComponent
-  }
-   path: 'dang-ky',
+  }, {
+    path: 'dang-ky',
     component: StudentGroupRegistrationComponent
   },
 ];
@@ -20,4 +18,5 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class StudentGroupRoutingModule { }
+export class StudentGroupRoutingModule {
+}
