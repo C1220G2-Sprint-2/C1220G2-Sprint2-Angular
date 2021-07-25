63d8771170a6761f05f8bf6f890dbd686b57b25e
thanhtam174 <jarentt17499@gmail.com>
2021-07-25 21:36:48 +0700

    update edit teacher

--- C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/app.module.ts
+++ C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/app.module.ts
@@ -1,22 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {SharedModule} from './shared/shared.module';
 import {TeacherModule} from './teacher/teacher.module';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {AngularFireStorageModule} from '@angular/fire/storage';
+import {environment} from '../environments/environment';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        SharedModule,
-        TeacherModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    TeacherModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule,
+    NgbModule // storage
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- C1220G2-Sprint2-Angular-Source/angular-sprint2/src/environments/environment.ts
+++ C1220G2-Sprint2-Angular-Source/angular-sprint2/src/environments/environment.ts
@@ -3,8 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  apiUrl: 'http://localhost:8080',
+  firebaseConfig: {
+    apiKey: "AIzaSyCBj5bHxpU5iqOHsb88k8Kv-k6620tMHmc",
+    authDomain: "project-management-b23f2.firebaseapp.com",
+    databaseURL: 'https://meeting-management-ec115-default-rtdb.firebaseio.com',
+    projectId: "project-management-b23f2",
+    storageBucket: "project-management-b23f2.appspot.com",
+    messagingSenderId: "957380437314",
+    appId: "1:957380437314:web:8696cf73472352d15354db",
+    measurementId: "G-NLN65GBMFB"
+  }
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
