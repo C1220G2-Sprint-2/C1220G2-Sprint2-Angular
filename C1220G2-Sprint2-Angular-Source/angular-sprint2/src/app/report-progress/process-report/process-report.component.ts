c91cec667e1b62926ea397134de1e0670ec01ad0
MoonHZ <58355523+tieusau12@users.noreply.github.com>
2021-07-26 08:59:58 +0700

    hand vs6

--- C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/report-progress/process-report/process-report.component.ts
+++ C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/report-progress/process-report/process-report.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {ReportServiceService} from '../report-service.service';
 import {ActivatedRoute, ParamMap, Router} from '@angular/router';
@@ -12,6 +12,8 @@ export class ProcessReportComponent implements OnInit {
   nameTopic: string;
   reportForm: FormGroup;
   name: string;
+  stages: number = 0;
+  @Input() backgroundColor: string = '#C2C2C2';
 
   constructor(private formBuilder: FormBuilder,
               private  reportService: ReportServiceService,
@@ -23,6 +25,14 @@ export class ProcessReportComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    console.log(this.stages);
+    if (this.stages < 26) {
+      this.backgroundColor = '#d91a37';
+    } else if (this.stages < 86) {
+      this.backgroundColor = '#103667';
+    } else {
+      this.backgroundColor = '#367517';
+    }
   }
 
 }
