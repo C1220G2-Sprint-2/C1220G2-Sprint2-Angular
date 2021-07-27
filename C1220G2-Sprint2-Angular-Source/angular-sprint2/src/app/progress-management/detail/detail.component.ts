651dda633e73110e31d2cfb51215360e7e9baf72
sangle173 <76678575+sangle173@users.noreply.github.com>
2021-07-27 22:28:25 +0700

    sangld update

--- C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/progress-management/detail/detail.component.ts
+++ C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/progress-management/detail/detail.component.ts
@@ -12,6 +12,8 @@ import {Observable} from 'rxjs';
 import {Announcement} from '../../models/announcement';
 import {AnnouncementService} from '../announcement.service';
 import {ToastrService} from 'ngx-toastr';
+import {ReviewDto} from "../review-dto";
+import {ProjectDto} from "../project-dto";
 
 @Component({
   selector: 'app-detail',
@@ -34,6 +36,14 @@ export class DetailComponent implements OnInit {
   isTeacherLogging = false;
   username: string;
   currentUsername: string;
+  accountName: string;
+  projectId: number;
+  reviewForm: FormGroup;
+  review: ReviewDto;
+  projectDto: ProjectDto;
+  reviewList: ReviewDto[];
+  isTeacherLogin = false;
+  userImage: string;
 
   constructor(private progressService: ProgressService,
               private activatedRoute: ActivatedRoute,
@@ -44,7 +54,7 @@ export class DetailComponent implements OnInit {
               private storage: AngularFireStorage,
               private toastService: ToastrService) {
     this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
-      this.name = paramMap.get('name');
+      this.projectId = +paramMap.get('id');
     });
   }
 
@@ -52,6 +62,7 @@ export class DetailComponent implements OnInit {
     this.avatar = this.tokenStorageService.getUser().avatar;
     this.currentUsername = this.tokenStorageService.getUser().name;
     this.getAllStudentDto();
+    console.log('all student ' + this.studentList.length);
     this.addNewConcernForm();
     this.addNewAnnouncementForm();
     this.getAnnouncementList();
@@ -61,16 +72,34 @@ export class DetailComponent implements OnInit {
       const user = this.tokenStorageService.getUser();
       this.username = user.username;
 
-      if (this.username.substring(0,2) == "TC") {
+      if (this.username.substring(0, 2) == "TC") {
         this.isTeacherLogging = true;
       }
     }
+    this.addNewReviewForm();
+    this.getAllReview();
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
+    if (this.isLoggedIn) {
+      const user = this.tokenStorageService.getUser();
+      this.username = user.username;
+      this.userImage = user.avatar;
+      this.accountName = user.name;
+      console.log(this.username);
+      if (this.username.substring(0, 2) === 'TC') {
+        this.isTeacherLogin = true;
+      }
+      console.log('hello teacher ' + this.isTeacherLogin);
+    }
+    this.progressService.getProjectById(this.projectId).subscribe(result => {
+      this.projectDto = result;
+    });
+    console.log('hello------------- ' + this.projectDto.id);
   }
 
   getAllStudentDto() {
     this.progressService.getAllStudentDto().subscribe(result => {
       this.studentList = result;
-      console.log('this' + result);
+      console.log('this' + result.length);
     });
   }
 
@@ -173,8 +202,56 @@ export class DetailComponent implements OnInit {
       );
   }
 
+  onSubmit() {
+    const reviewDto = this.reviewForm.value;
+    this.review = {
+      title: reviewDto.title,
+      content: reviewDto.content,
+      progressReview: reviewDto.progressReview,
+      teacherCode: this.tokenStorageService.getUser().username
+    };
+    console.log('dasdasdasd' + this.review.teacherCode);
+    console.log('this.gif do' + reviewDto);
+    this.progressService.addNewReview(this.review).subscribe(() => {
+      this.showSuccessReview();
+      // @ts-ignore
+      window.location.reload();
+    });
+  }
+
+  addNewReviewForm() {
+    this.reviewForm = new FormGroup({
+      title: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      content: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(250)]),
+      progressReview: new FormControl(0, [Validators.required]),
+      teacherCode: new FormControl('')
+    });
+  }
+
+  getAllReview() {
+    this.progressService.getAllReview().subscribe(result => {
+      this.reviewList = result;
+    });
+  }
+
   showSuccess() {
     this.toastService.success('', 'Thành công !');
+  }
+
+  showSuccessReview() {
+    this.toastService.success('Thành công !', 'Tạo đánh giá thành công');
+  }
+
+  get title() {
+    return this.reviewForm.get('title');
+  }
+
+  get content() {
+    return this.reviewForm.get('content');
+  }
+
+  get progressReview() {
+    return this.reviewForm.get('progressReview');
   }
 }
 
