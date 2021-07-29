650bbf0295804acf569a06924c728a469774c116
huyvmdn <76674566+huyvmdn@users.noreply.github.com>
2021-07-29 15:51:35 +0700

    Update team-management.component.ts

--- C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/student-group/team-management/team-management.component.ts
+++ C1220G2-Sprint2-Angular-Source/angular-sprint2/src/app/student-group/team-management/team-management.component.ts
@@ -1,16 +1,22 @@
 import {Component, OnInit} from '@angular/core';
 import {TeamService} from "../team.service";
 import {Router} from "@angular/router";
+
 import {TokenStorageService} from "../../security/token-storage.service";
-import {StudentGroupService} from "../student-group.service";
+import {StudentGroupService} from '../student-group.service';
+
 @Component({
   selector: 'app-team-management',
   templateUrl: './team-management.component.html',
   styleUrls: ['./team-management.component.css']
 })
 export class TeamManagementComponent implements OnInit {
+  page: any = 1;
+  pageSize: any = 8;
+
   listStudent: any = [];
   listTeam: any[] = [];
+
   private team = {
     name: "",
     enable: false,
@@ -18,25 +24,28 @@ export class TeamManagementComponent implements OnInit {
     listTeam: []
   };
   nameTeam: string = '';
+
   private user ;
   isLoggedIn: boolean = false;
   check:boolean = true;
+
   collection: any[] = this.listStudent;
-  pageCard: number = 1;
   studentTS?: any = {};
   project: any = {};
    teamDB: any ={};
-  pageCard2: number = 1;
    studentDelete: any= {};
-  page: any = 1;
-  pageSize: any = 8;
+
+
   constructor(  private tokenStorageService: TokenStorageService,private teamService: TeamService, private route: Router) {
   }
+
+
   ngOnInit(): void {
     this.isLoggedIn = !!this.tokenStorageService.getToken();
     if (this.isLoggedIn) {
       this.user = this.tokenStorageService.getUser();
     }
+
     if (this.user.username != null) {
       this.teamService.getStudent(this.user.username).subscribe(data => {
         console.log(data);
@@ -50,6 +59,7 @@ export class TeamManagementComponent implements OnInit {
           console.log(data);
           this.project=data;
         });
+
         this.teamService.listStudent().subscribe(data => {
           console.log(data);
           this.listStudent = data.filter(function (student) {
@@ -63,6 +73,7 @@ export class TeamManagementComponent implements OnInit {
           }
           console.log(newListStudent);
           this.listStudent= newListStudent;
+
           for(let i=0; i< newListStudent.length;i++) {
             if (newListStudent[i].groupStatus !=1) {
               this.check=false;
@@ -76,6 +87,7 @@ export class TeamManagementComponent implements OnInit {
       console.log(this.team)
     })
   }
+
   deleteStudent(s: any) {
     this.teamService.removeTeam(s).subscribe( data => {
       this.teamService.listStudent().subscribe(data => {
@@ -91,11 +103,14 @@ export class TeamManagementComponent implements OnInit {
         }
         console.log(newListStudent);
         this.listStudent= newListStudent;
+
       })
+
     })
   }
   guiThongtin(studentDelete: any) {
     console.log(studentDelete)
     this.studentDelete= studentDelete;
   }
+
 }
